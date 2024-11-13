af8f6ae08d83d1c183ca9a2bc51d5dbdb7635788
DivyaniCassionPrinston <divyanicassionprinston@gmail.com>
2024-11-13 10:14:00 +0530

    new files updated

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -1,15 +1,26 @@
+const ProductModel = require('../models/productModel');
+
 exports.getProducts = async (req, res, next) => {
     
+    const products = await ProductModel.find(query);
     res.json({
         success: true,
-        message:'get products'
+        products
     })
 }
 
-exports.getSingleProducts = async (req, res, next) => {
-    
-    res.json({
-        success: true,
-        message:'get single products'
-    })
+//Get Single Product API - /api/v1/product/:id
+exports.getSingleProduct = async (req, res, next) => {
+    try {
+        const product = await ProductModel.findById(req.params.id);
+        res.json({
+            success: true,
+            product
+        })
+    } catch (error) {
+        res.status(404).json({
+            success: false,
+            message: 'Unable to get Product with given ID'
+        })
+    }
 }
